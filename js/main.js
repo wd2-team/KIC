c3f4ed511d829132910dfd25a9d3bbf9fb414430
Jewelry <supirs102@gmail.com>
2021-11-14 16:46:53 +0900

    サイトの基礎

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(function(){
+    // スクロールでajax
+    $(window).scroll(function() {
+        console.log('windowスクロールなう');
+        if ($(this).scrollTop() > 0) {
+        $.ajax({
+            url: 'page00.html',
+            type: 'GET',
+            dataType: 'html'
+        }).done(function(data){
+            $('.result').html(data);
+        }).fail(function(data){
+            alert('通信失敗！');
+        });
+        }
+    });
+
+    // 動画再生後フェード
+    var video = document.getElementById('topvideo');
+    var btn_topskip = document.getElementById("btn_topskip");
+    video.addEventListener("ended", function() {
+        $('#topvideowrapper').fadeOut();
+        $('#topheader').fadeIn();
+        $('#main').fadeIn();
+        $('footer').fadeIn();
+    });
+    btn_topskip.addEventListener("click", (e) => {
+        video.currentTime += 999;
+    });
+});
