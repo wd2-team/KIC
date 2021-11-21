405b21e21b6d16fa7322155c54fb3e1c206bcb92
Jewelry <supirs102@gmail.com>
2021-11-22 01:55:34 +0900

    index基礎作成

--- js/main.js
+++ js/main.js
@@ -21,11 +21,36 @@ $(function(){
     var btn_topskip = document.getElementById("btn_topskip");
     video.addEventListener("ended", function() {
         $('#topvideowrapper').fadeOut();
-        $('#topheader').fadeIn();
-        $('#main').fadeIn();
+        $('header').fadeIn();
         $('footer').fadeIn();
+        $('#topheader').fadeIn();
+        setTimeout(function(){
+            $('#mainvideowrapper').fadeIn();
+        },3000);
+        setTimeout(function(){
+            $('#topheader h1').fadeIn(1000);
+            $('#topheader ul').fadeIn(1000);
+            $('#mainlogo').fadeIn(1000);
+            $('#footerleft').fadeIn(1000);
+            $('#footerright').fadeIn(1000);
+        },6000);
+        setTimeout(function(){
+            $('#topleft').fadeIn();
+            $('#topleft').addClass('active');
+        },8000);
+        setTimeout(function(){
+            $('#topright').fadeIn();
+            $('#topright').addClass('active');
+        },9000);
+        setTimeout(function(){
+            $('#main input').fadeIn(1000);
+        },10000);
     });
     btn_topskip.addEventListener("click", (e) => {
         video.currentTime += 999;
+    });
+
+    $(window).on('load', function(){
+
     });
 });
