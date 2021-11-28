c59ecc6fff8af72bfd8884ab7a279ad9d9f23eb6
Jewelry <supirs102@gmail.com>
2021-11-29 03:56:01 +0900

    vh変数確認

--- js/main.js
+++ js/main.js
@@ -101,6 +101,15 @@ $(function(){
         }
     };
 
+    // vh
+    const setVh = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    };
+     
+    window.addEventListener('load', setVh);
+    window.addEventListener('resize', setVh);
+
 // ロード
 $(window).on('load', function(){
 
